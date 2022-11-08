d4f39647d6a0c3a750f9928113f7edf3ddce7388
BellaBradbury <bellabradbury@yahoo.com>
2022-11-08 18:27:13 -0500

    findall added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const Book = require('../models/book.js').db;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  ( async() => {
+    const books = await Book.findAll();
+  })
 });
 
 module.exports = router;
