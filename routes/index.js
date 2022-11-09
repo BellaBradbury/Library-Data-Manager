f7279c45686f989b4c286ee9cafd14ca1dbc1eba
BellaBradbury <bellabradbury@yahoo.com>
2022-11-08 19:26:51 -0500

    book import try

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var Sequelize = require('sequelize');
+let sequelize = require('./models').sequelize;
+const Book = require('./models').book;
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'library.db'
-});
-
-// import book model
-const Book = require('.../models/book.js');
-
-Book.findAll().then( (books) => {
-  res.json(books);
-})
+// async handler
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(err){
+      res.status(500).send(err);
+    }
+  }
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  ( async() => {
-    const books = await Book.findAll();
-    return map(books).then( res => res.json() );
-  });
-});
+router.get('/', asyncHandler( async(req, res) => {
+  const books = await Book.findAll();
+  console.log(books);
+}));
 
 module.exports = router;
